441103130ce05c1b908d1e9f704474bc5519cd66
JSimmons0311 <joshua_simmons@protonmail.com>
2015-08-23 14:12:48 -0400

    Update FIND_TOA.m

--- FIND_TOA.m
+++ FIND_TOA.m
@@ -31,37 +31,4 @@ function TOA = FIND_TOA(d,td2,td3,td4,vP)
     
     TOA = TOA1;
 
-%     % Change of Sign Method
-% roots = zeros(1,4);
-%     i = 1;
-%     for TOA1 = 0:1e-3:TOA_Upper;
-%         TOA2 = TOA1+1e-3;
-%         if ( f(d,td2,td3,td4,TOA1,vP) * f(d,td2,td3,td4,TOA2,vP) < 0 )
-%             roots(i)   = TOA1;
-%             roots(i+1) = TOA2;
-%             i = i+2;
-%         end
-%     end
-%     
-%     % Getting ready for Bisector Method
-%     if ( roots(4) > roots (2) )
-%         TOA1 = roots(3);
-%         TOA2 = roots(4);
-%     elseif ( roots(4) < roots (2) )
-%         TOA1 = roots(1);
-%         TOA2 = roots(2);
-%     else
-%         TOA = -1;
-%     end
-    
-%     TOA_Test = 0:1e-3:TOA_Upper;
-%     Y = f(d,td2,td3,td4,TOA_Test,vP);
-%         figure(99)
-%         plot(TOA_Test,Y,'-r');
-%         hold on;
-%         line([TOA_Test(1),TOA_Test(end)],[0,0],'Color',[0,0,1]);
-%         plot(TOA,f(d,td2,td3,td4,TOA,vP),'g.','MarkerSize',20);
-%         string123 = sprintf('%3.3e', TOA);
-%         legend(string123);
-%         hold off;
 end
