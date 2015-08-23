1af1f6ff78f4c70120855a74bd3100062d7a7b87
JSimmons0311 <joshua_simmons@protonmail.com>
2015-08-23 14:28:11 -0400

    Update TDOA_3D_Tetrahedron_V4.m

--- TDOA_3D_Tetrahedron_V4.m
+++ TDOA_3D_Tetrahedron_V4.m
@@ -51,7 +51,7 @@ clc;
 
 % Global Simulation Parameters
 trialTotal = 100;  % Total number of iterations of main loop
-updateTime = 0.5;  % Delay after 1 complete iteration of main loop
+dwellTime = 0.5;  % Delay after 1 complete iteration of main loop
 fig1_On = true;    % Turn on/off visual containing raw time signals and XCs
 fig2_On = true;    % Turn on/off visual containing compass and source grid
 
@@ -286,5 +286,5 @@ for trialCount = 1:trialTotal;
                 hold off;
                 
     end
-    pause(updateTime);
+    pause(dwellTime);
 end
