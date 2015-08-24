d5dd04db5ad630b1f452e0643c82a09db72dd09b
JSimmons0311 <joshua_simmons@protonmail.com>
2015-08-23 21:31:16 -0400

    Create MAXIMUM2.m

---
+++ MAXIMUM2.m
@@ -0,0 +1,19 @@
+function [yMax, xMax] = MAXIMUM2(X,Y,i1,i2)
+    % This function calculates the maximum Y value (yMax) and its
+    % corresponding X location (xMax).
+    
+    if length(X) ~= length(Y);
+        error('X and Y are not the same length');
+    end
+    
+    yMax = 0;
+    
+    for i=i1:i2;
+       yTest = Y(i);
+       
+       if (yTest>yMax)
+           yMax = yTest;
+           xMax = i;
+       end
+    end
+end
