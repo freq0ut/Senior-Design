a2e4a549201168b54bc02c360f6e9f00cc69f394
JSimmons0311 <jps81@zips.uakron.edu>
2015-09-16 10:48:53 -0400

    Added C code to GitHub.

--- MATLAB/TDOA/TDOA_3D_Diamond_V1.m
+++ MATLAB/TDOA/TDOA_3D_Diamond_V1.m
@@ -70,7 +70,7 @@ addpath('/Users/betio32/Desktop/Senior-Design/MATLAB/Support_Functions');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Global Simulation Parameters
-trialTotal = 1E+2;    % Total number of iterations of main loop
+trialTotal = 1E+3;    % Total number of iterations of main loop
 dwellTime = 0.5;      % Delay after 1 complete iteration of main loop
 fig1_On = false;      % Turn on/off visual containing raw time signals and XCs
 fig2_On = true;       % Turn on/off visual containing compass and source grid
