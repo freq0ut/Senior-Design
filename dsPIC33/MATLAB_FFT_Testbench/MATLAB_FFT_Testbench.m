49067dbaa9c500eac74d4b4e18e80be8c196204e
JSimmons0311 <jps81@zips.uakron.edu>
2016-01-12 21:39:07 -0600

    Updated MATLAB FFT TestBench

--- dsPIC33/MATLAB_FFT_Testbench/MATLAB_FFT_Testbench.m
+++ dsPIC33/MATLAB_FFT_Testbench/MATLAB_FFT_Testbench.m
@@ -1,8 +1,9 @@
-% Joshua Simmons
+%   Joshua Simmons
 %
-% January 11, 2016
+%   January 11, 2016
 %
-% MATLAB Testbench for cross-checking Microchip MPLABX dsPIC30F FFT Example Code for a Square Wave Signal.
+%   MATLAB Testbench for cross-checking Microchip MPLABX dsPIC30F FFT
+%   Example Code for a 1kHz Square Wave Signal.
 
 close all;
 clear all;
@@ -17,29 +18,35 @@ fileID = fopen('sigCmpx.csv','r');% Open file
     CSV_File = textscan(fileID,'%s %s');% Read in entire CSV file into memory as a cell array
     
     N = size(CSV_File{1,1},1)/2;% Total number of samples
-    IQ_Data = zeros(1,N);% Declare and initialize IQ Data array
+    Y_DSPIC = zeros(1,N);% Declare and initialize IQ Data array
 
     for i = 0:N-1;
         % In-Phase
         tempCell = textscan(CSV_File{1,2}{2*i+1}(3:6),'%c');% Extract a specific cell in the cell array
         tempStringHex = char(tempCell);% Convert the cell to a string of characters (hexadecimal)
         tempBin = HEX_2_BIN(tempStringHex);% Convert the string of characters (hexadecimal) to integer (binary)
-        IQ_Data(i+1) = BIN_2_DEC(tempBin);% Convert the integer binary (2s Comp) to integer decimal 
+        Y_DSPIC(i+1) = BIN_2_DEC(tempBin);% Convert the integer binary (2s Comp) to integer decimal 
         
         % Quadrature
         tempCell = textscan(CSV_File{1,2}{2*i+2}(3:6),'%c');
         tempStringHex = char(tempCell);
         tempBin = HEX_2_BIN(tempStringHex);
-        IQ_Data(i+1) = IQ_Data(i+1) + 1i*BIN_2_DEC(tempBin);
+        Y_DSPIC(i+1) = Y_DSPIC(i+1) + 1i*BIN_2_DEC(tempBin);
     end
 
 fclose(fileID);% Close file
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%% TAKING THE INVERSE FFT OF THE MPLABX DSPIC30F DATA %%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+y_DSPIC = ifft(Y_DSPIC) * N;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%% CREATING MATLAB SIMULATED SIGNAL FOR COMPARISON %%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-fS = 1e3;% Sampling frequency [Hz]
+fS = 10e3;% Sampling frequency [Hz]
 Ts = 1/fS;% Sampling interval [s]
 
 f0 = 1/((N-1)*Ts);% Frequency resolution [Hz]
@@ -47,15 +54,16 @@ f0 = 1/((N-1)*Ts);% Frequency resolution [Hz]
 f = f0*(0:N-1);% Frequency Array (single-sided)
 t = Ts*(0:N-1);% Time Array
 
-% Creating the temporal square wave with 50% duty cycle 10 samples per period
-y = ones(1,N);
+% Creating a 1kHz square wave (50% duty cycle) with 10 samples per period
+% with ZERO DC offset.
+y_MATLAB = ones(1,N);
 for i=0:N-1;
     if (mod(i,10) >= 5)
-        y(i+1) = -1;
+        y_MATLAB(i+1) = -1;
     end
 end
 
-Y = fft(y) / N;% Taking the Fast-Fourier-Transform of the square wave
+Y_MATLAB = fft(y_MATLAB) / N;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%% PLOTTING %%%%%%%%%%
@@ -63,27 +71,36 @@ Y = fft(y) / N;% Taking the Fast-Fourier-Transform of the square wave
 
 fig1 = figure('units','normalized','outerposition',[0 0 1 1]);
     subplot(2,2,1);
-    	stem(t,y,'-b');
+    	stem(t,y_MATLAB,'-b');
     	grid on;
     	grid minor;
         xlim([t(1),t(end)]);
     	xlabel('Time [s]');
     	ylabel('Amplitude [V]');
-        titleString = sprintf('TEMPORAL\nMATLAB Simulated Input Signal');
+        titleString = sprintf('TEMPORAL\nMATLAB');
+        title(titleString);
+    subplot(2,2,2);
+    	stem(t,y_DSPIC,'-b');
+    	grid on;
+    	grid minor;
+        xlim([t(1),t(end)]);
+    	xlabel('Time [s]');
+    	ylabel('Amplitude [V]');
+        titleString = sprintf('TEMPORAL\ndsPIC');
         title(titleString);
     subplot(2,2,3);
-    	stem(f,abs(Y),'-r');
+    	stem(f,abs(Y_MATLAB),'-r');
     	grid on;
     	grid minor;
     	xlabel('Frequency [Hz]');
     	ylabel('Magnitude [Volts]');
-        titleString = sprintf('FREQUENCY\nMATLAB Simulated Input Signal');
+        titleString = sprintf('FREQUENCY\nMATLAB');
         title(titleString);
     subplot(2,2,4);
-    	stem(f,abs(IQ_Data)/2^14,'-r');
+    	stem(f,abs(Y_DSPIC),'-r');
     	grid on;
     	grid minor;
     	xlabel('Frequency [Hz]');
     	ylabel('Magnitude [Volts]');
-        titleString = sprintf('FREQUENCY\ndsPIC Simulated Output Data');
+        titleString = sprintf('FREQUENCY\ndsPIC');
         title(titleString);
